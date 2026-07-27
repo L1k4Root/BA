e3417db51a8d7e0ee815b664e370469ae29551b7
Andres <andres.antonio2000@hotmail.com>
2026-07-27 15:17:35 -0400

    fix: send contact forms through Google SMTP

---
+++ website/server/config.example.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'smtp' => [
+        'host' => 'smtp.gmail.com',
+        'port' => 587,
+        'username' => 'contacto@bachile.cl',
+        'app_password' => 'REPLACE_WITH_GOOGLE_APP_PASSWORD',
+    ],
+    'mail_to' => 'contacto@bachile.cl',
+    'allowed_origins' => [
+        'https://bachile.cl',
+        'https://www.bachile.cl',
+    ],
+];
